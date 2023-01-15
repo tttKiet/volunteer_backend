d4e251d8f89ee1d72ffd21b008628dc95d0a91bf
BuiKiet <roiyano1utl@gmail.com>
2023-01-15 20:12:50 +0700

    1. Babel, babel_env, morgan

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import { default as dotenv } from "dotenv";
+dotenv.config();
+
 const app = express();
-const port = 3000;
+
+app.use(morgan("combined"));
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello World! back end");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port  http://localhost:${port}`);
 });
